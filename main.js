7c03f5b8bcf395f77fc4f26e9e912e51d864943e
juniorCoderKrithika <72335062+juniorCoderKrithika@users.noreply.github.com>
2022-06-07 16:40:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ posenet.on('pose',gotPoses);
 
 function draw(){
 image(video,0,0,300,300);
-image(lips,noseX-30,noseY,50,30)
+image(lips,noseX-25,noseY+5,50,30)
 }
 function take_snapshot(){
 save("MyFilterSelfie.png")
